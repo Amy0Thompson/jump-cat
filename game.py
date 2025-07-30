2dc0d87c9d7a6652f005e9763e4774620d433e96
Amy Thompson <amythompson0325@gmail.com>
2025-07-29 20:49:02 -0400

    white game screen

---
+++ game.py
@@ -0,0 +1,34 @@
+import random #for RNG
+import pygame
+pygame.init()
+
+white = (255, 255, 255)
+black = (0 ,0, 0)
+#colors
+
+width = 500
+height = 500
+#dimensions
+
+score = 0
+player_x = 50
+player_y = 200
+fps = 60
+background = white
+timer = pygame.time.Clock()
+#game variables
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Jump Cat')
+#game window
+
+gameRunning = True
+while gameRunning:
+    timer.tick(fps)
+    screen.fill(background)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            gameRunning = False #prevent infinite loop / allow exit
+    pygame.display.flip()
+pygame.quit()
+    
