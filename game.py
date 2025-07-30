f727c6aae7ebe8709d834f88964c5a2c13237f3a
Amy Thompson <amythompson0325@gmail.com>
2025-07-29 23:49:46 -0400

    left & right movement with arrow keys

--- game.py
+++ game.py
@@ -16,6 +16,7 @@ score = 0
 player_x = 50
 player_y = 300
 y_change = 0
+x_change = 0
 gravity = 1
 fps = 60
 background = pygame.transform.scale(bg, (width, height))
@@ -40,6 +41,25 @@ while gameRunning:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and y_change == 0:
                 y_change = 18
+            if event.key == pygame.K_RIGHT:
+                x_change = 4
+            if event.key == pygame.K_LEFT:
+                x_change = -4
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                x_change = 0
+            if event.key == pygame.K_LEFT:
+                x_change = 0
+#movement
+
+    if 0 <= player_x <= 749:
+        player_x += x_change
+    if player_x < 0:
+        player_x = 0
+    if player_x > 650:
+        player_x = 650
+#boundaries
+                
     if y_change > 0 or player_y < 300:
         player_y -= y_change
         y_change -= gravity
@@ -47,7 +67,7 @@ while gameRunning:
         player_y = 300 #preventing glitching through floor
     if player_y == 300 and y_change < 0:
         y_change = 0
-#movement
+#jumping
             
     pygame.display.flip()
     
